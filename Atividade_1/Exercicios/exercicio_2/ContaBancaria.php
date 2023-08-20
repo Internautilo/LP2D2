bdd4996f68b2dd756426fcf6b987330849d84d56
Internautilo <yuribertulucci2004@gmail.com>
2023-08-20 15:49:31 -0300

    implementação da classe requerida no exercicio 2

---
+++ Atividade_1/Exercicios/exercicio_2/ContaBancaria.php
@@ -0,0 +1,41 @@
+<?php
+    class ContaBancaria{
+        private $titular;
+        private $numeroConta;
+        private $saldo;
+
+        function __construct($titular, $numeroConta)
+        {
+            $this->titular = $titular;
+            $this->numeroConta = $numeroConta;
+            
+            $this->saldo = rand(-500, 2000);
+        }
+
+        function get_titular()
+        {
+            return $this->titular;
+        }
+        function get_numeroConta()
+        {
+            return $this->numeroConta;
+        }
+        function get_saldo()
+        {
+            return $this->saldo;
+        }
+
+        function sacar($valor)
+        {
+            if($valor > $this->saldo){
+                return -1;
+            } else {
+                $this->saldo -= $valor;
+            }
+        }
+        function depositar($valor)
+        {
+            $this->saldo += $valor;
+        }
+    }
+?>
