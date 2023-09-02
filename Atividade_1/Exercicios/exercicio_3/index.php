4b86a0335376cdb30a13f1a01fb355c2fc31c3a4
Internautilo <yuribertulucci2004@gmail.com>
2023-09-01 21:20:25 -0300

    teste

--- Atividade_1/Exercicios/exercicio_3/index.php
+++ Atividade_1/Exercicios/exercicio_3/index.php
@@ -6,7 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atividade 1 - LP2D3</title>
-    <?php require_once './cabecalho.php'; ?>
+    <?php require_once './cabecalho.php'; 
+            error_reporting(1);
+    ?>
 </head>
 
 <body>
