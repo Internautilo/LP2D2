a74617814a265e38f29e72451a14edcc4caeec7c
Internautilo <yuribertulucci2004@gmail.com>
2023-11-13 15:02:14 -0300

    Created product migration table and updated users migration table

--- Laravel-Project/database/migrations/2023_11_13_171454_create_products_table.php
+++ Laravel-Project/database/migrations/2023_11_13_171454_create_products_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->boolean('is_admin')->default(0);
+            $table->longText('description');
+            $table->string('category');
+            $table->float('price', 8, 2);
+            $table->integer('stock_quantity');
+            $table->enum('status', ['active', 'inactive']);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
