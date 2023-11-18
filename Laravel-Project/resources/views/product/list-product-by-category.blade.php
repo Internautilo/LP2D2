57314652f61d8e1775c288e9a27ff048c451f0d2
Internautilo <yuribertulucci2004@gmail.com>
2023-11-18 12:50:19 -0300

    Added logic behind list-product-by-category view to show only the products from the selected category

--- Laravel-Project/resources/views/product/list-product-by-category.blade.php
+++ Laravel-Project/resources/views/product/list-product-by-category.blade.php
@@ -1,40 +1,35 @@
 @extends('layouts.app')
 
-@section('title', "Lista de {{ $category }}")
+@section('title', 'Lista de {{ $category }}')
 
 
 @section('content')
-    
+
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <div class="card my-5">
+                <div class="card my-5 col-12">
                     <div class="card-header">
                         <h4>Lista de Produtos: {{ ucfirst($category) }}</h4>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body text-center bg-light-subtle row justify-content-center align-items-center">
                         @foreach ($products as $product)
-                            
-                            @if ($product['category'] == $category)
-
-                            <div class="card">
-                                <div class="card-header">
-                                    {{ $product['name'] }}
+                            @if ($product['category'] == $category && $product['status'] == 'active')
+                                    
+                                <div class="card col-sm-3 bg-light text-dark my-2 mx-2">
+                                    <div class="card-header bg-dark-subtle text-light-emphasis">
+                                        {{ $product['name'] }}
+                                    </div>
+                                    <img src="/uploads/products/{{ $product['product_image'] }}" class="card-img-top">
+                                    <div class="card-body">
+                                        <p>{{ $product['description'] }}</p>
+                                        <p class="bg-light-subtle rounded rounded-5 text-success-emphasis">R$ {{ $product['price'] }}</p>
+                                    </div>
                                 </div>
-                            </div>
-                                
-                            @else
-                                
-                                <div class="text-center">
-                                    Produtos não encontrados nessa Categoria
-                                </div>
-
+                                    
                             @endif
-
                         @endforeach
                     </div>
                 </div>
-            </div>
         </div>
     </div>
 
--- Laravel-Project/resources/views/product/product-categories.blade.php
+++ Laravel-Project/resources/views/product/product-categories.blade.php
@@ -3,50 +3,49 @@
 @section('title', 'Categoria de Produtos')
 
 @section('content')
-<hr class="hr invisible my-5">
-<div class="container rounded-5 text-center pb-1 px-3 mb-5 text-primary-emphasis bg-dark-subtle border border-3 border-shadow">
-    <p class="pt-5 pb-1 my-2 col-12 bolder fs-1 text-center">Produtos</p>
-    <div class="container text-center mx-auto px-auto my-5 py-5 ">
-        <div class="row  align-items-center justify-content-evenly">
-            <div class="col-sm-3 card pt-2 bg-light text-dark border border-4 rounded rounded-4 border-shadow-3 ">
-                <div class="card-header bg-light">Copos</div>
-                <img src="{{ asset('img/cat-copo.webp') }}"
-                    class="img-card-top">
-                <div class="card-body">
-                    <h5 class="card-title">Copo plastico descartável</h5>
-                    <p class="card-text">Copos plásticos descartáveis em polipropileno (PP), ideais para eventos e
-                        festas. Leves, resistentes e ecologicamente corretos. Com capacidade de variadas, são a escolha
-                        perfeita para servir bebidas com praticidade e sustentabilidade.</p>
-                    <a href="{{ route('list_products', 'copo') }}" class="btn btn-primary">Ver mais</a>
+    <hr class="hr invisible my-5">
+    <div
+        class="container rounded-5 text-center pb-1 px-3 mb-5 text-primary-emphasis bg-dark-subtle border border-3 border-shadow">
+        <p class="pt-5 pb-1 my-2 col-12 bolder fs-1 text-center">Produtos</p>
+        <div class="container text-center mx-auto px-auto my-5 py-5 ">
+            <div class="row align-items-center justify-content-evenly">
+                <div class="col-sm-3 card pt-2 bg-light text-dark border border-4 rounded rounded-4 border-shadow-3 ">
+                    <div class="card-header bg-light">Copos</div>
+                    <img src="{{ asset('img/cat-copo.webp') }}" class="img-card-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Copo plastico descartável</h5>
+                        <p class="card-text">Copos plásticos descartáveis em polipropileno (PP), ideais para eventos e
+                            festas. Leves, resistentes e ecologicamente corretos. Com capacidade de variadas, são a escolha
+                            perfeita para servir bebidas com praticidade e sustentabilidade.</p>
+                        <a href="{{ route('list_products', 'copo') }}" class="btn btn-primary">Ver mais</a>
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm-3 card pt-2 bg-light text-dark border border-4 rounded rounded-4 border-shadow-3 ">
-                <div class="card-header bg-light">Tampas</div>
-                <img src="{{ asset('img/cat-tampa.webp') }}"
-                    class="img-card-top">
-                <div class="card-body">
-                    <h5 class="card-title">Tampa plástica descartável</h5>
-                    <p class="card-text">Tampas descartáveis em poliestireno (PS) com encaixe perfeito em nossos copos. Feitas para
-                        vedação segura, garantindo higiene e evitando derramamentos. Práticas para eventos, mantêm a
-                        qualidade das bebidas.</p>
-                    <a href="{{ route('list_products', 'tampa') }}" class="btn btn-primary">Ver mais</a>
+                <div class="col-sm-3 card pt-2 bg-light text-dark border border-4 rounded rounded-4 border-shadow-3 ">
+                    <div class="card-header bg-light">Tampas</div>
+                    <img src="{{ asset('img/cat-tampa.webp') }}" class="img-card-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Tampa plástica descartável</h5>
+                        <p class="card-text">Tampas descartáveis em poliestireno (PS) com encaixe perfeito em nossos copos.
+                            Feitas para
+                            vedação segura, garantindo higiene e evitando derramamentos. Práticas para eventos, mantêm a
+                            qualidade das bebidas.</p>
+                        <a href="{{ route('list_products', 'tampa') }}" class="btn btn-primary">Ver mais</a>
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm-3 card pt-2 bg-light text-dark border border-4 rounded rounded-4 border-shadow-3 ">
-                <div class="card-header bg-light">Pratos</div>
+                <div class="col-sm-3 card pt-2 bg-light text-dark border border-4 rounded rounded-4 border-shadow-3 ">
+                    <div class="card-header bg-light">Pratos</div>
 
-                <img src="{{ asset('img/cat-prato.jpg') }}"
-                    class="img-card-top">
-                <div class="card-body">
-                    <h5 class="card-title">Prato plástico descartável</h5>
-                    <p class="card-text">Pratos descartáveis em poliestireno (PS) combinam praticidade e resistência.
-                        Com design elegante, são ideais para eventos e festas. Leves e duráveis, facilitam o serviço de
-                        refeições, proporcionando uma experiência sempre agradável.</p>
-                    <a href="{{ route('list_products', 'prato') }}" class="btn btn-primary">Ver mais</a>
+                    <img src="{{ asset('img/cat-prato.jpg') }}" class="img-card-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Prato plástico descartável</h5>
+                        <p class="card-text">Pratos descartáveis em poliestireno (PS) combinam praticidade e resistência.
+                            Com design elegante, são ideais para eventos e festas. Leves e duráveis, facilitam o serviço de
+                            refeições, proporcionando uma experiência sempre agradável.</p>
+                        <a href="{{ route('list_products', 'prato') }}" class="btn btn-primary">Ver mais</a>
+                    </div>
                 </div>
+
             </div>
-            
         </div>
     </div>
-</div>
 @endsection
