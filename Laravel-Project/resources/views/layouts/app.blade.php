324261e1d2014dfc5a8137aa5657d7e9ba881748
Internautilo <yuribertulucci2004@gmail.com>
2023-11-20 14:24:08 -0300

    Added a minor change on the navbar when there is a section active

--- Laravel-Project/resources/views/layouts/app.blade.php
+++ Laravel-Project/resources/views/layouts/app.blade.php
@@ -2,65 +2,91 @@
 <html lang="en" data-bs-theme="dark">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>@yield('title')</title>
-  <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
-  <script defer src="{{ asset('/js/bootstrap.js') }}"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+    <script defer src="{{ asset('/js/bootstrap.bundle.js') }}"></script>
 </head>
 
 
 <body>
 
 
-  {{-- Navbar --}}
-  <nav>
-    <header
-      class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3  mx-1 border-bottom">
-      <div class="col-md-3 mb-2 mb-md-0">
-        <a href="{{ route('home') }}" class="d-inline-flex  ms-2 mt-1 ">
-          <img src="{{ asset('img/Lojinha_logo.png') }}" class="px-2 py-2 bg-info-subtle img-thumbnail" style="height: 3rem">
-        </a>
-      </div>
-      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2">Produtos</a></li>
-        <li><a href="{{ route('categories') }}" class="nav-link px-2">Catalogo</a></li>
-        <li><a href="#" class="nav-link px-2">Sobre nós</a></li>
-        <li><a href="#" class="nav-link px-2">Contato</a></li>
-      </ul>
+    {{-- Navbar --}}
+    <nav>
+        <header
+            class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3  mx-1 border-bottom">
+            <div class="col-md-3 mb-2 mb-md-0">
+                <a href="{{ route('home') }}" class="d-inline-flex  ms-2 mt-1 ">
+                    <img src="{{ asset('img/Lojinha_logo.png') }}" class="px-2 py-2 bg-info-subtle img-thumbnail"
+                        style="height: 3rem">
+                </a>
+            </div>
+            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2">Produtos</a></li>
+                <li><a href="{{ route('categories') }}" class="nav-link px-2">Catalogo</a></li>
+                <li><a href="#" class="nav-link px-2">Sobre nós</a></li>
+                <li><a href="#" class="nav-link px-2">Contato</a></li>
+            </ul>
 
-      <div class="col-md-3 text-end">
-        <a href="{{ route('user_login') }}" class="btn btn-outline-primary me-2">Login</a>
-        <a href="{{ route('user_signup') }}" class="btn btn-primary">Cadastro</a>
-      </div>
-    </header>
-  </nav>
-  </nav>
+            <div class="col-md-3 text-end">
 
-  {{-- Content --}}
-  <div class=" min-vh-100">
-    @yield('content')
-  </div>
+                @if (Auth::check())
+                    @php
+                        $user = Auth::user();
+                    @endphp
+                    <div class="dropdown">
+                        <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            {{ $user->name }}
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a href="" class="dropdown-item">Conta</a></li>
+                            <li>
+                                <div class="dropdown-divider"></div>
+                            </li>
+                            <li><a href="#" class="dropdown-item">Sair</a></li>
+                        </ul>
+                    </div>
+                @else
+                    <a href="{{ route('user_login') }}" class="btn btn-outline-primary me-2">Login</a>
+                    <a href="{{ route('user_signup') }}" class="btn btn-primary">Cadastro</a>
+                @endif
 
-  {{-- Footer --}}
-  <footer>
-    <div class="container-fluid">
-      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <p class="col-md-4 mb-0 text-body-secondary">© 2023 Lojinha, Inc</p>
+            </div>
+        </header>
+    </nav>
+    </nav>
 
-        <div class="col-md-4 text-center mt-2" style="height: 4rem">
-          <img src="{{ asset('img/Lojinha_logo.png') }}" class="bg-light-subtle img-thumbnail py-2 px-2" style="height: 4rem" >
-        </div>
-
-        <ul class="nav col-md-4 justify-content-end">
-          <li class="nav-item"><a href="{{ route('home') }}" class="nav-link px-2 text-body-secondary">Home</a></li>
-          <li class="nav-item"><a href="{{ route('categories') }}" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-          <li class="nav-item"><a href="{{ url('/sobre_nos') }}" class="nav-link px-2 text-body-secondary">About</a></li>
-        </ul>
-      </footer>
+    {{-- Content --}}
+    <div class=" min-vh-100">
+        @yield('content')
     </div>
-  </footer>
+
+    {{-- Footer --}}
+    <footer>
+        <div class="container-fluid">
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <p class="col-md-4 mb-0 text-body-secondary">© 2023 Lojinha, Inc</p>
+
+                <div class="col-md-4 text-center mt-2" style="height: 4rem">
+                    <img src="{{ asset('img/Lojinha_logo.png') }}" class="bg-light-subtle img-thumbnail py-2 px-2"
+                        style="height: 4rem">
+                </div>
+
+                <ul class="nav col-md-4 justify-content-end">
+                    <li class="nav-item"><a href="{{ route('home') }}"
+                            class="nav-link px-2 text-body-secondary">Home</a></li>
+                    <li class="nav-item"><a href="{{ route('categories') }}"
+                            class="nav-link px-2 text-body-secondary">Pricing</a></li>
+                    <li class="nav-item"><a href="{{ url('/sobre_nos') }}"
+                            class="nav-link px-2 text-body-secondary">About</a></li>
+                </ul>
+            </footer>
+        </div>
+    </footer>
 </body>
 
 </html>
