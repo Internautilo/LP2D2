dca2cd5a26df7c052a8cc0d245518ec0b56ad7db
Internautilo <yuribertulucci2004@gmail.com>
2023-11-22 11:45:29 -0300

    Added the basic template for products in list-products view

--- Laravel-Project/resources/views/product/list-products.blade.php
+++ Laravel-Project/resources/views/product/list-products.blade.php
@@ -4,5 +4,52 @@
 
 @section('content')
 
-    
+<div class="container" data-bs-theme="light">
+    <hr class="vr invisible">
+    <div class="card">
+        <div class="card-body bg-dark-subtle">
+            <div class="card">
+                <div class="card-header bg-dark-subtle">
+                    <h4>Lista de produtos</h4>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <div class="col-sm-3">
+                                <div class="card bg-light text-dark my-2 mx-2">
+                                    <div class="card-header bg-dark-subtle text-light-emphasis">
+                                        Nome produto
+                                    </div>
+                                    <img src="/uploads/products/" class="card-img-top">
+                                    <div class="card-body">
+                                        <div class="row justify-content-center align-items-center">
+                                            <div class="col-sm-6 d-flex justify-content-center">
+                                                <form action="" method="post">
+                                                    <button type="submit" class="btn btn-primary">
+                                                        Editar
+                                                    </button>
+                                                </form>
+                                            </div>
+                                            <div class="col-sm-6 d-flex justify-content-center">
+                                                <form action="" method="post">
+                                                    <button type="submit" class="btn btn-danger">
+                                                        Deletar
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+</div>
+
 @endsection
--- Laravel-Project/routes/web.php
+++ Laravel-Project/routes/web.php
@@ -57,7 +57,7 @@ Route::get('/categorias', function() {
 Route::get('/inserir_produto', function () {
     return view('product.insert-product');
 })->name('product_insertion_form');
-Route::get('/listar_produtos/{category?}', function( ?string $category) {
+Route::get('/produtos/{category?}', function( ?string $category) {
     if(isset($category)){
         $products = Product::all();
         return view('product.list-product-by-category')->with(['category' => $category, 'products' => $products]);
@@ -65,6 +65,9 @@ Route::get('/listar_produtos/{category?}', function( ?string $category) {
         return view('product-categories');
     }
 })->name('list_products');
+Route::get('/listar_produtos', function() {
+    return view('product.list-products');
+})->name('list_all_products');
 
     // POST
 Route::post('/inserir_produto', [ProductController::class, 'store'])->name('insert_product');
